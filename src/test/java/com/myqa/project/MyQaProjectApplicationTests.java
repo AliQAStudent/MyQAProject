2b896820857462b76a7868bbce7c922e65408633
Javier Desktop <alija2794@outlook.com>
2022-03-04 12:15:09 +0000

    first commit

---
+++ src/test/java/com/myqa/project/MyQaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myqa.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyQaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
